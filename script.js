789f4dd86f542db2ac10fb45ab0b63446823df76
durgeshraut540 <durgeshraut111@gmail.com>
2024-12-27 16:26:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById('addFriendBtn').addEventListener('click', function() {
+    alert('Add Friend functionality will be implemented later.');
+});
+
+document.getElementById('viewSummaryBtn').addEventListener('click', function() {
+    alert('View Summary functionality will be implemented later.');
+});
